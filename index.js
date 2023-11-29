1859ac01ca6b0eeaaf8b1531436f58473a5a1180
Matthew Gsell <matthewdavidgsell@gmail.com>
2023-11-29 05:49:03 -0500

    create board factory started creating players

--- index.js
+++ index.js
@@ -1,38 +1,99 @@
 export function testfunction() {
-    return 4
+  return 4;
 }
 
-
 export function ship(name, length) {
-    return {
-        name: name,
-        length: length,
-        hits: 0,
-        hit: function() {
-            this.hits++
-        },
-        isSunk: function() {
-            if (this.hits >= this.length){
-                return true
-            }else {
-                return false
-            }
-
-        }
-    }
+  return {
+    name: name,
+    length: length,
+    hits: 0,
+    hit: function () {
+      this.hits++;
+    },
+    isSunk: function () {
+      if (this.hits >= this.length) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  };
 }
 
 export function gameboard(name) {
-    return {
-        name: name,
-        shiplist: [],
-        xcorlist: [0, 1, 2, 3, 4, 5, 6, 7],
-        ycorlist: [0, 1, 2, 3, 4, 5, 6, 7],
-        placeship: function(name, length, shiphead, shiptail) {
-            let newship = ship(name, length)
-            newship.cordonates = [[0, 1], [0, 2], [0, 3]]
-            this.shiplist.push(newship)
+  return {
+    name: name,
+    shiplist: [],
+    coordonates:
+      // prettier-ignore
+      [[0, 7], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7],
+      [0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 6],
+    [0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5],
+    [0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4],
+    [0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3],
+    [0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2],
+    [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1],
+    [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],],
+    hitattacks: [],
+    missedattacks: [],
 
+    placeship: function (name, length, shiphead, shiptail) {
+      let newship = ship(name, length);
+      newship.coordonates = [];
+      if (shiphead[0] === shiptail[0]) {
+        for (let i = 0; i < newship.length; i++) {
+          newship.coordonates.push([shiphead[0], shiphead[1] + i]);
         }
-    }
+      } else if (shiphead[1] === shiptail[1]) {
+        for (let i = 0; i < newship.length; i++) {
+          newship.coordonates.push([shiphead[0] + i, shiphead[1]]);
+        }
+      }
+
+      this.shiplist.push(newship);
+    },
+    receiveAttack: function (position) {
+      this.shiplist.forEach((element) => {
+        element.coordonates.forEach((cor) => {
+          if (cor[0] === position[0] && cor[1] === position[1]) {
+            element.hit();
+            this.hitattacks.push(position);
+            return;
+          }
+        });
+      });
+      if (!this.hitattacks.includes(position)) {
+        this.missedattacks.push(position);
+      }
+    },
+    allShipsSunk: function () {
+      let sunkships = 0;
+      this.shiplist.forEach((element) => {
+        if (element.isSunk() === true) {
+          sunkships++;
+        }
+      });
+      if (sunkships === this.shiplist.length) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  };
+}
+
+export function Player(name) {
+  return {
+    name: name,
+    board: gameboard(name + "s board"),
+    attackEnemy: ComputerOne.board.receiveAttack(position),
+  };
+}
+
+export function Computer(name) {
+  return {
+    name: name,
+    board: gameboard(name + "s board"),
+    attackEnemy: PlayerOne.board.receiveAttack(position),
+  };
 }
--- index.test.js
+++ index.test.js
@@ -1,36 +1,99 @@
-import { testfunction, ship, gameboard  } from "./index";
+import { experiments } from "webpack";
+import { testfunction, ship, gameboard, Player, Computer } from "./index";
 
-test('checking if this works' , () => {
-    expect(testfunction()).toBe(4)
-})
+test("checking if this works", () => {
+  expect(testfunction()).toBe(4);
+});
 
+test("shipcreation", () => {
+  expect(ship("Carrier", 5).length).toBe(5);
+});
 
-test('shipcreation', () => {
-    expect(ship('Carrier', 5).length).toBe(5)
-})
+test("ship gets hit", () => {
+  const carrier = ship("Carrier", 5);
+  carrier.hit();
+  carrier.hit();
+  expect(carrier.hits).toBe(2);
+});
 
-test('ship gets hit', () => {
-    const carrier = ship('Carrier', 5)
-    carrier.hit()
-    carrier.hit()
-    expect(carrier.hits).toBe(2)
+test("is ship sunk", () => {
+  const carrier = ship("Carrier", 5);
+  carrier.hit();
+  carrier.hit();
+  expect(carrier.isSunk()).toBe(false);
+  carrier.hit();
+  carrier.hit();
+  carrier.hit();
+  expect(carrier.isSunk()).toBe(true);
+});
 
-})
+test("is ship placed on grid in vertical orientation", () => {
+  const board = gameboard();
+  board.placeship("Carrier", 5, [0, 1], [0, 5]);
+  expect(board.shiplist[0].coordonates).toEqual([
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+    [0, 5],
+  ]);
+});
 
-test('is ship sunk', () => {
-    const carrier = ship('Carrier', 5)
-    carrier.hit()
-    carrier.hit()
-    expect(carrier.isSunk()).toBe(false)
-    carrier.hit()
-    carrier.hit()
-    carrier.hit()
-    expect(carrier.isSunk()).toBe(true)
+test("is ship placed on grid in horizontal orientation", () => {
+  const board = gameboard();
+  board.placeship("Carrier", 5, [1, 0], [5, 0]);
+  expect(board.shiplist[0].coordonates).toEqual([
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+    [5, 0],
+  ]);
+});
 
-})
+test("did ship get hit via gameboard", () => {
+  const board = gameboard();
+  board.placeship("Carrier", 5, [1, 0], [5, 0]);
+  board.receiveAttack([3, 0]);
+  board.receiveAttack([4, 0]);
+  board.receiveAttack([5, 0]);
+  board.receiveAttack([7, 0]);
+  board.receiveAttack([6, 0]);
+  expect(board.shiplist[0].hits).toBe(3);
+});
 
-test('is ship placed on grid', () => {
-    const board = gameboard() 
-    board.placeship('Submarine', 3, [0, 1], [0, 3])
-    expect(board.shiplist[0].cordonates).toEqual([[0, 1], [0, 2], [0,3]])
-})
+test("does the board keep track of missed and hit shots", () => {
+  const board = gameboard();
+  board.placeship("Destroyer", 2, [0, 1], [0, 2]);
+  board.receiveAttack([0, 1]);
+  board.receiveAttack([0, 2]);
+  board.receiveAttack([0, 3]);
+  board.receiveAttack([0, 4]);
+  expect(board.hitattacks).toEqual([
+    [0, 1],
+    [0, 2],
+  ]);
+  expect(board.missedattacks).toEqual([
+    [0, 3],
+    [0, 4],
+  ]);
+});
+
+test("board function that declares if all ships are sunk", () => {
+  const board = gameboard();
+  board.placeship("Destroyer", 2, [0, 1], [0, 2]);
+  board.receiveAttack([0, 1]);
+  board.receiveAttack([0, 2]);
+  expect(board.allShipsSunk()).toBe(true);
+  board.placeship("Destroyer", 2, [0, 4], [0, 5]);
+  expect(board.allShipsSunk()).toBe(false);
+});
+
+test("user can attack computer and computer can attack user", () => {
+  const PlayerOne = Player("Player");
+  const ComputerOne = Computer("Player");
+  PlayerOne.placeship("Destroyer", 2, [0, 1], [0, 2]);
+  ComputerOne.placeship("Destroyer", 2, [0, 1], [0, 2]);
+  PlayerOne.attackEnemy([0, 1]);
+  expect(Computer.board.shiplist[0].hits).toBe(1);
+});
